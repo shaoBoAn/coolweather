5f58270c1b8d95ff566937908ae9078a5b4fc46f
crethdom <crethdom@163.com>
2017-06-15 23:34:49 +0800

    create database and table

---
+++ app/src/main/java/com/coolweather/android/db/Province.java
@@ -0,0 +1,32 @@
+package com.coolweather.android.db;
+
+import javax.management.openmbean.CompositeDataSupport;
+
+/**
+ * Created by pc on 2017/6/15.
+ */
+
+public class Province extends DataSupport {
+    private  int id;
+    private  String provinceName;
+    private  int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public String getProvinceName() {
+        return provinceName;
+    }
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
