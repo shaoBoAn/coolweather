b5cfa376ad29915e75f6a9b455a5b7d892373dfd
crethdom <crethdom@163.com>
2017-06-19 23:13:54 +0800

    add json code

--- app/src/main/java/com/coolweather/android/util/Utility.java
+++ app/src/main/java/com/coolweather/android/util/Utility.java
@@ -1,8 +1,38 @@
 package com.coolweather.android.util;
 
+import android.text.TextUtils;
+
+import com.coolweather.android.db.Province;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Created by pc on 2017/6/17.
  */
 
 public class Utility {
+    /**
+     * 解析和处理服务器返回的省级数据
+     */
+    public static boolean handProvinceResponse(String response){
+        if(!TextUtils.isEmpty(response)){
+            try{
+                JSONArray allProvinces = new JSONArray(response);
+                for(int i =0;i<allProvinces.length();i++){
+                    JSONObject provinceObject = allProvinces.getJSONObject(i);
+                    Province province = new Province();
+                    province.setProvinceName(provinceObject.getString("name"));
+                    province.getProvinceCode(provinceObject.getInt("id"));
+                    province.save();
+                }
+                return true;
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
 }
